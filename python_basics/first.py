63afc5db4e9ed645cadd4e2373beb4f0fe8c4928
Youssef Lagzouli <ylagzoul@e1r3p5.1337.ma>
2025-12-12 15:12:12 +0100

    python

---
+++ python_basics/first.py
@@ -0,0 +1,31 @@
+print("hello pythone with youssef")
+
+
+print("\n---------------------------\n")
+# ---------------------------------
+# what is type()
+# All Data in python is Object
+# ---------------------------------
+
+print(type(10)) # int
+print(type(-100)) # int
+print(type(10.201)) # float
+print(type("youssef")) # string
+print(type([1,2,5,8])) # list
+print(type((1,2,3,4,5,10))) # tuple
+print(type( {"one" : 1 ,"tow" : 2} )) # dict => dictionary
+print(type(2 == 2)) # bool => boolean --> true folst   
+
+print("\n---------------------------\n")
+
+Myvaryble = "youssef is good\n"
+Age = 20
+print(Myvaryble)
+print(Age)
+
+print("\n---------------------------\n")
+# Concatenation
+
+name1 = "YOUSSEF"
+name2 = "LAGZOULI"
+print(name1 + " " + name2)
